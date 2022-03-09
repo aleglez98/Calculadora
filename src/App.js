887a51d5a9d2ce3da3b3c9add8e0c8bb4588fc9a
Alejandro González González <agongon@arte-consultores.com>
2022-03-09 19:09:12 +0000

    Conseguido guardar en la barra el numero que queremos

--- src/App.js
+++ src/App.js
@@ -1,32 +1,10 @@
 import './App.css';
-import './Components/NumberButton/NumberButton';
-import NumberButton from './Components/NumberButton/NumberButton';
+import Cuadricula from './Components/Cuadricula/Cuadricula';
 
 function App() {
   return (
     <div className="App">
-        <NumberButton number="AC" id="AC" />
-        <NumberButton number="C" />
-        <NumberButton number="%" />
-        <NumberButton number="/" />
-        <br />
-        <NumberButton number="7" />
-        <NumberButton number="8" />
-        <NumberButton number="9" />
-        <NumberButton number="*" />
-        <br />
-        <NumberButton number="4" />
-        <NumberButton number="5" />
-        <NumberButton number="6" />
-        <NumberButton number="-" />
-        <br />
-        <NumberButton number="1" />
-        <NumberButton number="2" />
-        <NumberButton number="3" />
-        <NumberButton number="+" />
-        <br />
-        <NumberButton number="0" />
-        <NumberButton id="igual" number="=" />
+        <Cuadricula />
     </div>
   );
 }
